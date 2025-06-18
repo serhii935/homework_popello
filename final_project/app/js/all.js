3f5c565fe58216abfc1b10b18b34a1c89ada1b20
Сергій Попелло <serhii@MacBook-Pro-Sergij.local>
2025-06-18 22:12:50 +0200

    page_about_adaptive

--- final_project/app/js/all.js
+++ final_project/app/js/all.js
@@ -4018,11 +4018,22 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
     return o;
   };
 });
-$('.js-slider').slick({
-  infinite: true,
-  dots: true,
-  arrows: false,
-  dotsClass: 'slider__dots',
-  slidesToShow: 1,
-  slidesToScroll: 1
+$('.js-slider, .js-slider-about').each(function () {
+  var $slider = $(this);
+  var isAbout = $slider.hasClass('js-slider-about');
+  $slider.slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    dotsClass: 'slider__dots',
+    slidesToShow: isAbout ? 4 : 1,
+    slidesToScroll: isAbout ? 2 : 1,
+    responsive: isAbout ? [{
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }] : []
+  });
 });
--- final_project/app/js/script.js
+++ final_project/app/js/script.js
@@ -1,9 +1,22 @@
+$('.js-slider, .js-slider-about').each(function () {
+  const $slider = $(this);
+  const isAbout = $slider.hasClass('js-slider-about');
 
-$('.js-slider').slick({
-  infinite: true,
-  dots: true,
-  arrows: false,
-  dotsClass:'slider__dots',
-  slidesToShow: 1,
-  slidesToScroll: 1
+  $slider.slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    dotsClass: 'slider__dots',
+    slidesToShow: isAbout ? 4 : 1,
+    slidesToScroll: isAbout ? 2 : 1,
+    responsive: isAbout ? [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ] : []
+  });
 });
